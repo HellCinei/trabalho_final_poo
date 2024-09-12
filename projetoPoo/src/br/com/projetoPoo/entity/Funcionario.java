f9dc7da10c3971d6a4c175a8433c146c3ff30160
DaviSoaresA <didequinho@gmail.com>
2024-09-12 17:16:25 -0300

    mudança funcionario

--- projetoPoo/src/br/com/projetoPoo/entity/Funcionario.java
+++ projetoPoo/src/br/com/projetoPoo/entity/Funcionario.java
@@ -1,7 +1,15 @@
 package br.com.projetoPoo.entity;
 
-public class Funcionario extends Pessoa{
+public class Funcionario extends Pessoa {
 	private Double salarioBruto;
 	private Double descontoInss;
 	private Double descontoIR;
+
+	public Funcionario(Double salarioBruto, Double descontoInss, Double descontoIR) {
+		super();
+		this.salarioBruto = salarioBruto;
+		this.descontoInss = descontoInss;
+		this.descontoIR = descontoIR;
+	}
+
 }

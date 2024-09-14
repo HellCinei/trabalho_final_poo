d6d88fe35514a75788b726b8039122c3b8e2750f
DaviSoaresA <didequinho@gmail.com>
2024-09-14 17:06:49 -0300

    Mudanças exceções

--- projetoPoo/src/br/com/projetoPoo/entity/Arquivo.java
+++ projetoPoo/src/br/com/projetoPoo/entity/Arquivo.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 import br.com.projetoPoo.enums.Parentesco;
 
 public class Arquivo {
-
+	private Verify v = new Verify();
 	private List<Funcionario> funcionarios = new ArrayList<>();
 
 	public List<Funcionario> getFuncionarios() {
@@ -45,9 +45,12 @@ public class Arquivo {
 
 						funcionarios.add(new Funcionario(dado, dado1, LocalDate.parse(dado2, formatter),
 								Double.parseDouble(dado3)));
+						v.validCpf(dado1);
 
 					} catch (NumberFormatException e) {
 
+						v.validCpf(dado1);
+						v.validAge(LocalDate.parse(dado2, formatter));
 						Dependente dependente = new Dependente(dado, dado1, LocalDate.parse(dado2, formatter),
 								Parentesco.valueOf(dado3));
 						if (!funcionarios.isEmpty()) {
@@ -70,6 +73,8 @@ public class Arquivo {
 
 		} catch (IOException e) {
 			System.err.println("Arquivo não encontrado!");
+		} catch (DependenteException e) {
+			System.err.println(e.getMessage());
 		} catch (Exception e) {
 			System.err.println(e.getMessage() + "Erro!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			e.printStackTrace();
@@ -79,7 +84,7 @@ public class Arquivo {
 	public void DigitarArquivo() {
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter(
-					"C:/Users/angel/Documents/trabalho_final_poo/projetoPoo/src/br/com/projetoPoo/testemain/planilhas/saidafuncionario.csv"));
+					"C:\\Users\\Win\\Documents\\trabalho_final_poo\\projetoPoo\\src\\br\\com\\projetoPoo\\testemain\\planilhas\\saidafuncionario.csv"));
 
 			for (Funcionario f : funcionarios) {
 				bw.write(f.toString());
@@ -87,6 +92,9 @@ public class Arquivo {
 			bw.close();
 		} catch (IOException e) {
 			System.err.println("O Arquivo não foi encontrado!");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("Erro!!!!");
 			e.printStackTrace();
 		}
 	}
